78f0c8f2a543fbb3e39f46fd4307e7e739ca14d8
DmytroOleksandrovych <@mail>
2018-09-26 00:11:18 +0300

    no message

---
+++ Assets/Scrips/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //[SerializeField]
+    Generator generator;
+
+    //рандомная скорость
+    int randSpeed;
+
+    void Start()
+    {
+        randSpeed = Random.Range(1, 10);
+        generator = FindObjectOfType<Generator>();
+    }
+
+    void Update()
+    {
+        //transform.position = Vector2.MoveTowards(transform.position, transform.up, Time.deltaTime*5);
+
+        transform.Translate(new Vector2(0, 1) * (Time.deltaTime * randSpeed));
+    }
+
+    private void OnBecameInvisible()
+    {
+        generator.CreateBall();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scrips/Generator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    [SerializeField]
+    [Header("Максимальное кол-во шаров")]
+    int maxBall;
+
+    [SerializeField]
+    [Header("Шары для генерации")]
+    GameObject[] arrayBall;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void CreateBall()
+    {
+        Instantiate(arrayBall[0], transform.position, Quaternion.identity);
+    }
+}
