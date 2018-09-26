4c324f8e8c14f493056e99ac4369d7ce4aa81b49
DmytroOleksandrovych <@mail>
2018-09-26 16:32:09 +0300

    no message

---
+++ Assets/Scrips/TouchTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchTest : MonoBehaviour
+{
+    [SerializeField]
+    Text text;
+
+    int i=0; 
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount>0|| Input.GetKeyDown(KeyCode.I))
+        {
+            i++;
+            text.text = i.ToString();
+        }
+    }
+}
